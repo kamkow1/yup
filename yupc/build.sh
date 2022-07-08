0ef909495500fd9354a7b135db2761c00add6cf8
kamkow1 <ikamileq@gmail.com>
2022-07-08 11:15:22 +0200

    add llvm to project

--- yupc/build.sh
+++ yupc/build.sh
@@ -1,3 +1,11 @@
+if [[ -d build ]]; then
+  rm -r build
+fi
+
+if [[ ! -d build ]]; then
+  mkdir build
+fi
+
 cd build/ &&
 conan install .. &&
 cmake .. &&
--- yupc/src/ast_visitor/visitor.h
+++ yupc/src/ast_visitor/visitor.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "../parser/YupParserBaseVisitor.h"
+#include "llvm/IR/IRBuilder.h"
 
-class Visitor : public YupParserBaseVisitor 
+class Visitor : public YupParserBaseVisitor
 {
 public:
+    llvm::Value *codegen();
+
     std::any visitFunc_def(YupParser::Func_defContext *ctx) override;
 
     std::any visitFile(YupParser::FileContext *ctx) override;
