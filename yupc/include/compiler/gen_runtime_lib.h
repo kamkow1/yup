dc70402de0a6ffa04de427fbea7d0f1b509b5c9f
kamkow1 <ikamileq@gmail.com>
2022-08-19 15:22:54 +0200

    add GC_malloc() and free() to runtime lib

--- yupc/include/compiler/gen_runtime_lib.h
+++ yupc/include/compiler/gen_runtime_lib.h
@@ -6,7 +6,11 @@ namespace yupc::compiler::gen_runtime_lib {
 
     void init_runtime_lib(llvm::Module &mod);
 
+    void init_gc_malloc(llvm::Module &mod);
+
     void init_malloc(llvm::Module &mod);
 
     void init_printf(llvm::Module &mod);
+
+    void init_free(llvm::Module &mod);
 }
--- yupc/src/compiler/gen_runtime_lib.cpp
+++ yupc/src/compiler/gen_runtime_lib.cpp
@@ -6,6 +6,11 @@
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Type.h>
 
+#define PRINTF_NAME     "printf"
+#define GC_MALLOC_NAME  "GC_malloc"
+#define MALLOC_NAME     "malloc"
+#define FREE_NAME       "free"
+
 using namespace yupc;
 using namespace llvm;
 
@@ -18,10 +23,10 @@ void grlib::init_printf(Module &mod) {
 
     auto *printf_ft = FunctionType::get(i32t, i8ptr_t, true);
 
-    mod.getOrInsertFunction("printf", printf_ft);
+    mod.getOrInsertFunction(PRINTF_NAME, printf_ft);
 }
 
-void grlib::init_malloc(Module &mod) {
+void grlib::init_gc_malloc(Module &mod) {
 
     auto *i8_t = Type::getInt8Ty(mod.getContext());
     auto *void_ptr_t = PointerType::get(i8_t, 0);
@@ -29,11 +34,35 @@ void grlib::init_malloc(Module &mod) {
 
     auto  *malloc_ft = FunctionType::get(void_ptr_t, i64_t, false);
 
-    mod.getOrInsertFunction("malloc", malloc_ft);
+    mod.getOrInsertFunction(GC_MALLOC_NAME, malloc_ft);
+}
+
+void grlib::init_malloc(llvm::Module &mod) {
+
+    auto *i8_t = Type::getInt8Ty(mod.getContext());
+    auto *void_ptr_t = PointerType::get(i8_t, 0);
+    auto *i64_t = Type::getInt64Ty(mod.getContext());
+
+    auto  *gc_malloc_ft = FunctionType::get(void_ptr_t, i64_t, false);
+
+    mod.getOrInsertFunction(MALLOC_NAME, gc_malloc_ft);
+}
+
+void grlib::init_free(Module &mod) {
+
+    auto *i8_t = Type::getInt8Ty(mod.getContext());
+    auto *void_ptr_t = PointerType::get(i8_t, 0);
+
+    auto *void_t = Type::getVoidTy(mod.getContext());
+
+    auto *free_ft = FunctionType::get(void_t, void_ptr_t, false);
+
+    mod.getOrInsertFunction(FREE_NAME, free_ft);
 }
 
 void grlib::init_runtime_lib(Module &mod) {
 
-    init_printf(mod);
-    init_malloc(mod);
+    grlib::init_printf(mod);
+    grlib::init_malloc(mod);
+    grlib::init_free(mod);
 }
