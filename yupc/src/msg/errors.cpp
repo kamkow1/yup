c3688a1923cdd1ecc0e2a42b08f988d1a2318bcf
kamkow1 <ikamileq@gmail.com>
2022-08-24 17:56:48 +0200

    remove termcolor

--- yupc/src/msg/errors.cpp
+++ yupc/src/msg/errors.cpp
@@ -1,93 +1,60 @@
 #include <msg/errors.h>
-#include <termcolor/termcolor.hpp>
 #include <iostream>
 
 void yupc::log_compiler_err(std::string detail, std::string text)
 {
     std::cout
-        << termcolor::red
         << "["
-        << termcolor::underline
         << "COMPILER ERROR"
-        << termcolor::reset
-        << termcolor::red
         <<"]: "
-        << termcolor::reset
         << detail
-        << termcolor::reset
         << "\n"
-        << termcolor::underline
         << text
-        << termcolor::reset
         << "\n";
 }
 
 void yupc::log_input_err(std::string detail)
 {
     std::cout
-        << termcolor::magenta
         << "["
-        << termcolor::underline
         << "INPUT ERROR"
-        << termcolor::reset
-        << termcolor::magenta
         <<"]: "
-        << termcolor::reset
         << detail
-        << termcolor::reset
         << "\n";
 }
 
 void yupc::log_parsing_err(std::string detail, std::string f_path)
 {
     std::cout
-        << termcolor::yellow
         << "["
-        << termcolor::underline
         << "PARSING ERROR"
-        << termcolor::reset
-        << termcolor::yellow
         << "]: "
-        << termcolor::reset
         << " (in file "
         << f_path
         << ") "
         << detail
-        << termcolor::reset
         << "\n";
 }
 
 void yupc::log_lexing_err(std::string detail, std::string f_path) {
     std::cout
-        << termcolor::cyan
         << "["
-        << termcolor::underline
         << "LEXING ERROR"
-        << termcolor::reset
-        << termcolor::yellow
         << "]: "
-        << termcolor::reset
         << " (in file "
         << f_path
         << ") "
         << detail
-        << termcolor::reset
         << "\n";
 }
 
 void yupc::log_cmd_err(std::string detail)
 {
     std::cout
-    << termcolor::yellow
     << "["
-    << termcolor::underline
     << "CMD ERROR"
-    << termcolor::reset
-    << termcolor::yellow
     << "]: "
-    << termcolor::reset
     << detail
-    << termcolor::reset
     << "\n";
     
 }
--- yupc/src/msg/info.cpp
+++ yupc/src/msg/info.cpp
@@ -1,19 +1,12 @@
 #include <msg/info.h>
-#include <termcolor/termcolor.hpp>
 #include <iostream>
 
 void yupc::log_cmd_info(std::string detail)
 {
     std::cout
-        << termcolor::bright_blue
         << "["
-        << termcolor::underline
         << "INFO"
-        << termcolor::reset
-        << termcolor::bright_blue
         << "]: "
-        << termcolor::reset
         << detail
-        << termcolor::reset
         << "\n";
 }
