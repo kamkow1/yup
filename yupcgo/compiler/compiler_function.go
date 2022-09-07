fc6b815a6313b8b38f248a05c6f323be094a9b9a
kamkow1 <ikamileq@gmail.com>
2022-09-07 18:06:54 +0200

    put all casting into a single cast function

--- yupcgo/compiler/compiler_function.go
+++ yupcgo/compiler/compiler_function.go
@@ -9,12 +9,10 @@ import (
 type functionType func([]string) llvm.Value
 
 var functions map[string]functionType = map[string]functionType{
-	"intCast":  intCast,
-	"bitCast":  bitCast,
-	"ptrToInt": ptrToIntCast,
+	"Cast": Cast,
 }
 
-func intCast(args []string) llvm.Value {
+func Cast(args []string) llvm.Value {
 	value := FindLocalVariable(args[0], len(CompilationUnits.Peek().Locals)-1).Value
 	load := CompilationUnits.Peek().Builder.CreateLoad(value, "")
 	typ, ok := BuiltinLLVMTypes[args[1]]
@@ -22,31 +20,17 @@ func intCast(args []string) llvm.Value {
 		panic(fmt.Sprintf("ERROR: unknown type: %s", args[1]))
 	}
 
-	return CompilationUnits.Peek().Builder.CreateIntCast(load, typ, "")
-}
-
-func bitCast(args []string) llvm.Value {
-	value := FindLocalVariable(args[0], len(CompilationUnits.Peek().Locals)-1).Value
-	load := CompilationUnits.Peek().Builder.CreateLoad(value, "")
-	typ, ok := BuiltinLLVMTypes[args[1]]
-	if !ok {
-		panic(fmt.Sprintf("ERROR: unknown type: %s", args[1]))
+	if load.Type().TypeKind() == llvm.IntegerTypeKind && typ.TypeKind() == llvm.IntegerTypeKind {
+		return CompilationUnits.Peek().Builder.CreateIntCast(load, typ, "")
+	} else if load.Type().TypeKind() == llvm.IntegerTypeKind && typ.TypeKind() == llvm.PointerTypeKind {
+		return CompilationUnits.Peek().Builder.CreateIntToPtr(load, typ, "")
+	} else if load.Type().TypeKind() == llvm.PointerTypeKind && typ.TypeKind() == llvm.IntegerTypeKind {
+		return CompilationUnits.Peek().Builder.CreatePtrToInt(load, typ, "")
+	} else if load.Type().TypeKind() == llvm.PointerTypeKind && typ.TypeKind() == llvm.PointerTypeKind {
+		return CompilationUnits.Peek().Builder.CreatePointerCast(load, typ, "")
+	} else {
+		return CompilationUnits.Peek().Builder.CreateBitCast(load, typ, "")
 	}
-
-	return CompilationUnits.Peek().Builder.CreateBitCast(load, typ, "")
-}
-
-func ptrToIntCast(args []string) llvm.Value {
-	value := FindLocalVariable(args[0], len(CompilationUnits.Peek().Locals)-1).Value
-	load := CompilationUnits.Peek().Builder.CreateLoad(value, "")
-	typ, ok := BuiltinLLVMTypes[args[1]]
-	if !ok {
-		panic(fmt.Sprintf("ERROR: unknown type: %s", args[1]))
-	}
-
-	fmt.Println(value.Type().String())
-	fmt.Println(typ.String())
-	return CompilationUnits.Peek().Builder.CreatePtrToInt(load, typ, "")
 }
 
 func CallBuiltinFunction(name string, args []string) llvm.Value {
