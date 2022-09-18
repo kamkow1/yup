f9c9aa33d54505154a9ed24b7dca453444832bc9
kamkow1 <ikamileq@gmail.com>
2022-09-19 00:21:52 +0200

    add scan_input() to stdlib

--- yup_stdlib/io.c
+++ yup_stdlib/io.c
@@ -31,3 +31,34 @@ void fmt_print(char *format, ...) {
 
     va_end(ap);
 }
+
+void scan_input(const char *format, ...) {
+	va_list ap;
+	    va_start(ap, format);
+	
+	    const char *ptr = format;
+	
+	    while(*ptr) {
+	        if (*ptr == '%') {
+	            ptr++;
+	            switch (*ptr++) {
+	                case 's':
+	                    scanf("%s", va_arg(ap, char*));
+	                    break;
+	                case 'i':
+	                    scanf("%d", va_arg(ap, int*));
+	                    break;
+	                case 'f':
+	                    scanf("%f", va_arg(ap, float*));
+	                    break;
+	                case 'c':
+	                    scanf("%c", va_arg(ap, char*));
+	                    break;
+	            }
+	        } else {
+	            putchar(*ptr++);
+	        }
+	    }
+	
+	    va_end(ap);
+}
